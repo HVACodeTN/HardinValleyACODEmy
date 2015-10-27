a0a4a819858ac202826430c91a1950f309ba24d4
Tymethy <kootra6@gmail.com>
2015-10-27 18:11:34 -0400

    Test image added to Map_Schedule
    Test image added.

--- apps/hvcodecademy/public/Map_Schedule.php
+++ apps/hvcodecademy/public/Map_Schedule.php
@@ -32,7 +32,19 @@ $D101="TeacherName" + " D101";
 
 
 
-<img src="" width="" height="" alt="Schedule"/>
+<TABLE BORDER="0" cellpadding="0" CELLSPACING="0">
+<TR>
+<!-- add img map img to background later (when I can scan it) place holder img for now to use for testing.-->
+<TD width="415" HEIGHT="300" VALIGN="Center" BACKGROUND="Header.jpg" Id="" alt="Schedule">
+
+<FONT SIZE="3" COLOR="Black" Id="$D101">D101</FONT>
+<br>
+<font size="3" color="black">D102</font>
+</TD>
+
+</TR>
+</TABLE>
+
 
 </body>
 </html>
