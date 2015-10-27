a82aeeaf74b878a4ca3288d6b89d872971065c4f
Tymethy <kootra6@gmail.com>
2015-10-27 16:15:09 -0400

    Created a map_schedule file
    The map schedule file will show a map with text overlaid where teachers
    are
    Right now just a template

---
+++ apps/hvcodecademy/public/Map_Schedule.php
@@ -0,0 +1,38 @@
+<?php
+
+
+require("common.php");
+
+//TODO: Jackson create variable for each room with "TeacherName" + "Room name" from database based on current time.
+// Rooms include All Classrooms, Labs, Workrooms, Multipurpose room, Library, bus duty (It's a place on the map)
+$D101="TeacherName" + " D101";
+
+
+//Put text on map
+
+
+
+
+
+
+
+
+?>
+<!doctype html>
+<html>
+<head>
+
+<meta charset="utf-8">
+
+
+</head>
+
+<body>
+
+
+
+
+<img src="" width="" height="" alt="Schedule"/>
+
+</body>
+</html>
