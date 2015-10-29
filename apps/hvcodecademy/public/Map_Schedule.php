cd7af52ace3eb1c64ea7bd17dbe9867ae6c66c3d
rjsmith1999 <rjsmith1999@gmail.com>
2015-10-29 17:33:05 -0400

    Added DB access code to Map_Schedule

--- apps/hvcodecademy/public/Map_Schedule.php
+++ apps/hvcodecademy/public/Map_Schedule.php
@@ -1,14 +1,49 @@
 <?php
 
 
-require("common.php");
+    require("common.php");
 
-//TODO: Jackson create variable for each room with "TeacherName" + "Room name" from database based on current time.
-// Rooms include All Classrooms, Labs, Workrooms, Multipurpose room, Library, bus duty (It's a place on the map)
-$D101="TeacherName" + " D101";
+    //TODO: Jackson create variable for each room with "TeacherName" + "Room name" from database based on current time.
+    // Rooms include All Classrooms, Labs, Workrooms, Multipurpose room, Library, bus duty (It's a place on the map)
+    $D101="TeacherName" + " D101";
 
+    //TODO: Logic to chose Period
+    $period = 1;
 
-//Put text on map
+    //Create Query
+    $query = "SELECT
+                Name,
+                Room
+            FROM Schedule
+            WHERE
+                Period = :Period";
+    $query_params = array( 
+        ':Period' => $period;
+    );
+
+    $num_results = 0;
+    try 
+    { 
+        // Execute the query against the database 
+        $stmt = $db->prepare($query); 
+        $result = $stmt->execute($query_params);
+        $num_results = $result->num_rows;
+    } 
+    catch(PDOException $ex) 
+    { 
+        //display if failed to run 
+        die("Failed to run query: " . $ex->getMessage()); 
+    }
+
+    
+    // Iterate Results
+    for ($i=0; $i < $num_results; $i++) { 
+        //Process Results
+        $row = $stmt->fetch();
+        // Use data to create variable for schedule
+    }
+    
+    //Put text on map
 
 
 
