8b61a5babc2868bfddf626ac207b4702d081be7e
rjsmith1999 <rjsmith1999@gmail.com>
2015-11-03 18:36:25 -0500

    Created Template index file
    Created a index file with private and common access.

--- apps/hvcodecademy/public/Index.php
+++ apps/hvcodecademy/public/Index.php
@@ -1,10 +1,46 @@
+<?php
+
+//connects to server
+require("common.php");
+
+//privates the page
+require("privte.php");
+
+
+
+
+
+
+?>
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
+
 <title>Untitled Document</title>
+
 </head>
 
 <body>
+	<!-- HEADER AND NAVBAR -->
+       <img src="../Header.jpg" width="1280" height="427" alt=""/>
+<!-- change logo later to updated verison instead of last year's -->
+            
+            <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/">Angular Routing Example</a>
+                </div>
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="../views/Login.php"> Login</a></li>
+                    <li><a href="../views/Register.php"> Register</a></li>
+                    <li><a href="../views/memberlist.php"> Admin</a></li>
+                    <li><a href="../views/edit_account.php"> My Account</a></li>
+                    <li><a href="../views/common.php"> common</a></li>
+                </ul>
+            </div>
+</nav>
+</header>
 </body>
 </html>
