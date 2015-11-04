3d1b4a807ba733a6ca83c5eda344425d4d065555
rjsmith1999 <rjsmith1999@gmail.com>
2015-11-03 19:30:35 -0500

    Fixed Login Redirect
    Set address for login.php file correctly

--- apps/hvcodecademy/public/views/private.php
+++ apps/hvcodecademy/public/views/private.php
@@ -7,7 +7,7 @@
     if(empty($_SESSION['user'])) 
     { 
         // If they are not, we redirect them to the login page. 
-        header("Location: Login.php"); 
+        header("Location: /views/Login.php"); 
          
         // Remember that this die statement is absolutely critical.  Without it, 
         // people can view your members-only content without logging in. 
