0b2f51de1e41545a0214f3db634015c483cbad74
Tymethy <kootra6@gmail.com>
2015-11-10 17:29:49 -0500

    Added Date Selector and Submit Button
    Added Date Selector and Submit Button

--- apps/hvcodecademy/public/views/RoomSignUp.php
+++ apps/hvcodecademy/public/views/RoomSignUp.php
@@ -69,6 +69,9 @@ if ($_SESSION['user']['AccountType']=='Teacher') {
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <!-- CUSTOM STYLE  -->
     <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- DATA SELECTOR BOOTSTRAPS -->
+    <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="screen" href="http://tarruda.github.com/bootstrap-datetimepicker/assets/css/bootstrap-datetimepicker.min.css">
     <!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -132,6 +135,32 @@ if ($_SESSION['user']['AccountType']=='Teacher') {
                         </datalist>
                     </fieldset>
                     <br>
+                    <fieldset>
+                    	<label for="">Date:</label>
+                        	<div id="datetimepicker" class="input-append date">
+      							<input type="text"></input>
+      								<span class="add-on">
+        								<i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
+      								</span>
+   							</div>
+					<script type="text/javascript"
+                        src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js">
+                    </script> 
+                    <script type="text/javascript"
+                        src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js">
+                    </script>
+                    <script type="text/javascript"
+                        src="http://tarruda.github.com/bootstrap-datetimepicker/assets/js/bootstrap-datetimepicker.min.js">
+                    </script>
+                    <script type="text/javascript"
+                        src="http://tarruda.github.com/bootstrap-datetimepicker/assets/js/bootstrap-datetimepicker.pt-BR.js">
+                    </script>
+                    <script type="text/javascript">
+                      $('#datetimepicker').datetimepicker({
+                        format: 'dd/MM/yyyy hh:mm:ss',
+                        language: 'pt-BR'
+                      });
+                    </script>
                 </form>
             </div>
             <div class="panel-body">
