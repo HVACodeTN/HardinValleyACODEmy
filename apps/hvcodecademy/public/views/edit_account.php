292107bd6945871e1f9096fc7162144b13128963
rjsmith1999 <rjsmith1999@gmail.com>
2015-11-09 22:19:08 -0500

    Some updates to edit account

--- apps/hvcodecademy/public/views/edit_account.php
+++ apps/hvcodecademy/public/views/edit_account.php
@@ -20,53 +20,7 @@
     // This if statement checks to determine whether the edit form has been submitted
     // If it has, then the account updating code is run, otherwise the form is displayed 
     if(!empty($_POST)) 
-    { 
-        // Make sure the user entered a valid E-Mail address 
-        if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) 
-        { 
-            die("Invalid E-Mail Address"); 
-        } 
-         
-        // If the user is changing their E-Mail address, we need to make sure that 
-        // the new value does not conflict with a value that is already in the system. 
-        // If the user is not changing their E-Mail address this check is not needed.
-        if($_POST['email'] != $_SESSION['user']['email']) 
-        { 
-            // Define our SQL query 
-            $query = " 
-                SELECT 
-                    1 
-                FROM Users 
-                WHERE 
-                    email = :email 
-            "; 
-             
-            // Define our query parameter values 
-            $query_params = array( 
-                ':email' => $_POST['email'] 
-            ); 
-             
-            try 
-            { 
-                // Execute the query 
-                $stmt = $db->prepare($query); 
-                $result = $stmt->execute($query_params); 
-            } 
-            catch(PDOException $ex) 
-            { 
-                // Note: On a production website, you should not output $ex->getMessage(). 
-                // It may provide an attacker with helpful information about your code.  
-                die("Failed to run query: " . $ex->getMessage()); 
-            } 
-             
-            // Retrieve results (if any) 
-            $row = $stmt->fetch(); 
-            if($row) 
-            { 
-                die("This E-Mail address is already in use"); 
-            } 
-        } 
-         
+    {  
         // If the user entered a new password, we need to hash it and generate a fresh salt 
         // for good measure. 
         if(!empty($_POST['password'])) 
@@ -87,42 +41,18 @@
          
         // Initial query parameter values 
         $query_params = array( 
-            ':email' => $_POST['email'], 
-            ':user_id' => $_SESSION['user']['id'], 
+            ':user_id' => $_SESSION['user']['UserID'], 
+            ':password' => $password,
+            ':salt' => $salt 
         ); 
          
-        // If the user is changing their password, then we need parameter values 
-        // for the new password hash and salt too. 
-        if($password !== null) 
-        { 
-            $query_params[':password'] = $password; 
-            $query_params[':salt'] = $salt; 
-        } 
-         
-        // Note how this is only first half of the necessary update query.  We will dynamically 
-        // construct the rest of it depending on whether or not the user is changing 
-        // their password. 
         $query = " 
-            UPDATE users 
+            UPDATE Users 
             SET 
-                email = :email 
-        "; 
-         
-        // If the user is changing their password, then we extend the SQL query 
-        // to include the password and salt columns and parameter tokens too. 
-        if($password !== null) 
-        { 
-            $query .= " 
-                , password = :password 
-                , salt = :salt 
-            "; 
-        } 
-         
-        // Finally we finish the update query by specifying that we only wish 
-        // to update the one record with for the current user. 
-        $query .= " 
+                Password = :password,
+                salt = :salt
             WHERE 
-                UserID = :user_id 
+                UserID = :user_id
         "; 
          
         try 
@@ -137,36 +67,24 @@
             // It may provide an attacker with helpful information about your code.  
             die("Failed to run query: " . $ex->getMessage()); 
         } 
-         
-        // Now that the user's E-Mail address has changed, the data stored in the $_SESSION 
-        // array is stale; we need to update it so that it is accurate. 
-        $_SESSION['user']['email'] = $_POST['email']; 
-         
+          
         // This redirects the user back to the members-only page after they register 
-        header("Location: private.php"); 
+        header("Location: index.php"); 
          
         // Calling die or exit after performing a redirect using the header function 
         // is critical.  The rest of your PHP script will continue to execute and 
         // will be sent to the user if you do not die or exit. 
-        die("Redirecting to private.php"); 
+        die("Redirecting to index.php"); 
     } 
      
 ?> 
 <h1>Edit Account</h1> 
 
-<!-- add admin only to Admin tab later -->
-<li><a href="../views/memberlist.php"> Admin</a></li>
-
 <form action="edit_account.php" method="post"> 
     Username:<br /> 
-    <b><?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?></b> 
-    <br /><br /> 
-    E-Mail Address:<br /> 
-    <input type="text" name="email" value="<?php echo htmlentities($_SESSION['user']['email'], ENT_QUOTES, 'UTF-8'); ?>" /> 
+    <b><?php echo htmlentities($_SESSION['user']['UserName'], ENT_QUOTES, 'UTF-8'); ?></b> 
     <br /><br /> 
     Password:<br /> 
-    <input type="password" name="password" value="" /><br /> 
-    <i>(leave blank if you do not want to change your password)</i> 
-    <br /><br /> 
+    <input type="password" name="password" value="" /><br /><br />
     <input type="submit" value="Update Account" /> 
 </form>
