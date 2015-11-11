af83356e5af7cfff3b457940a1f79f6897ab6897
Sean Toll <smtdoo@comcast.net>
2015-11-11 13:31:22 -0500

    Fixes to about.php

--- apps/hvcodecademy/public/views/about.php
+++ apps/hvcodecademy/public/views/about.php
@@ -39,7 +39,7 @@
               <br>
               <h2>About the Project</h2>
               <p>
-              To solve this year's problem, we created a school scheduling and map to help schools 
+              To do this, we created a school schedule and map to help schools 
               keep track of things around school, such as laptop carts and room assignments.
               </p>
               <br>
@@ -52,37 +52,44 @@
                 </div>
               <p><br>
                 
+          <!--  <div class="container"
+            <div class="row row-centered"
             <div class="col-md-2 text-center">
                 <div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
                   <div class="caption">
-                    <h3> Tym Brandel
-                      <br>
-                      <small>Team Leader</small> </h3>
-                    <p>Tym primarily spent his time working on HTML code when he was not guiding the team as a whole.</p>
-            <!-- 	      <ul class="list-inline">
-                      <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a> </li>
-                      <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a> </li>
-                      <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a> </li>
-                    </ul> -->
-                    </div>
+                    
+                      <center><h3> Tym Brandel
+                        <br>
+                        <small>Team Leader</small> </h3>
+                        <p>Tym primarily spent his time working on HTML code when he was not guiding the team as a whole.</p>
+            <! 	      <ul class="list-inline">
+                        <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a> </li>
+                        <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a> </li>
+                        <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a> </li>
+                      </ul> -->
+                    
+              <!--      </div>
                   </div>
-                <br>
-                </p>
-              <br><br><br>
-              </div>
+                  <br>
+                </p> 
+              </center>
+            <br><br><br>
+            </div>
+          </div>
               
-              <div class="col-sm-2 text-left">
+              <div class="row row-centered">
+              <div class="col-sm-2 text-center">
                   <div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
                     <div class="caption">
                       <h3> Jackson Smith
                         <br>
                         <small>PHP Extraordinairre</small> </h3>
                       <p>Jackson was the master of the PHP arts, filling out the database with style.</p>
-            <!--   	      <ul class="list-inline">
+               	      <ul class="list-inline">
                         <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a> </li>
                         <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a> </li>
                         <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a> </li>
-                      </ul> -->
+                      </ul> 
                       </div>
                     </div>
                   <br>
@@ -90,45 +97,47 @@
                 <br><br><br>
                 </div>
                 
-                <div class="col-sm-2 text-left">
+                <div class="col-sm-2 text-center">
                     <div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
                       <div class="caption">
                         <h3> Sean Toll
                           <br>
                           <small>CSS Person</small> </h3>
                         <p>Sean made the websites presentable in a time when they looked bland.</p>
-            <!--     	      <ul class="list-inline">
+                 	      <ul class="list-inline">
                           <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a> </li>
                           <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a> </li>
                           <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a> </li>
-                        </ul> -->
+                        </ul> 
                         </div>
                       </div>
                     <br>
-            <!--     	  </p> -->
+                 	  </p> 
                   <br><br><br>
                   </div>
+                </div>
                   
-                  <div class="col-sm-2 text-left">
+                  <div class="row row-centered">
+                  <div class="col-sm-2 text-center">
                       <div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
                         <div class="caption">
                           <h3> Joey Townsend
                             <br>
                             <small>Database Person</small> </h3>
                           <p>Jpey assisted Jackson in his PHP objectives.</p>
-            <!--       	      <ul class="list-inline">
+                   	      <ul class="list-inline">
                             <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a> </li>
                             <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a> </li>
                             <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a> </li>
                           </ul> -->
-                          </div>
+            <!--              </div>
                         </div>
                       <br>
-            <!--       	  </p> -->
+                   	  </p> 
                     <br><br><br>
                     </div>
                     
-                    <div class="col-sm-2 text-left">
+                    <div class="col-sm-2 text-center">
                         <div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
                           <div class="caption">
                             <h3> Jack Anderson
@@ -146,9 +155,103 @@
                         </p>
                       <br><br><br>
                       </div>
+                    </div>
+                  </div>
               
               </center>
+            </div> -->
+            
+            <div class="container">
+              <div class="row row-centered">
+                <div class="col-xs-12 col-centered">
+                  <center><div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    <div class="caption">
+                        <h3> Tym Brandel
+                          <br>
+                          <small>Team Leader</small> </h3>
+                          <p>Tym primarily spent his time working on HTML code when
+                             he was not guiding the team as a whole.
+                          </p>
+                        </div>
+                            </div>
+                            <br>
+                          </p> </center> 
+                      <br><br><br>
+                </div>
+              <div class="row row-centered">
+                <div class="col-xs-6 col-centered">
+                  <center><div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    <div class="caption">
+                        <h3> Jackson Smith
+                          <br>
+                          <small>PHP Extraordinaire</small> </h3>
+                          <p>Jackson was the master of the PHP arts, filling out the databases in style.
+                          </p>
+                        </div>
+                            </div>
+                            <br>
+                          </p> </center> 
+                      <br><br><br>
+                </div>
+                <div class="col-xs-6 col-centered">
+                  <center><div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    <div class="caption">
+                        <h3> Sean Toll
+                          <br>
+                          <small>CSS Connoisseur.</small> </h3>
+                          <p>Sean made the websites presentable in a time when they looked bland.
+                          </p>
+                        </div>
+                            </div>
+                            <br>
+                          </p> </center> 
+                      <br><br><br>
+                </div>
+              </div>
+              <div class="row row-centered">
+                <div class="col-xs-6 col-centered">
+                  <center><div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    <div class="caption">
+                        <h3> Joey Townsend
+                          <br>
+                          <small>Database Delinquent</small> </h3>
+                          <p>Joey assisted Jackson in his PHP objectives.
+                          </p>
+                        </div>
+                            </div>
+                            <br>
+                          </p> </center> 
+                      <br><br><br>
+                </div>
+                <div class="col-xs-6 col-centered">
+                  <center><div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    <div class="caption">
+                        <h3> Jack Anderson
+                          <br>
+                          <small>HTML Editor</small> </h3>
+                          <p>Jack was there, too.
+                          </p>
+                        </div>
+                            </div>
+                            <br>
+                          </p> </center> 
+                      <br><br><br>
+                    </div>
+                </div>
+                <div class="row row-centered">
+                <div class="col-xs-12 col-centered">
+                  <center>
+                    <ul class="list-inline">
+                      <li><a href="https://www.facebook.com/"><i class="fa fa-2x fa-facebook-square"></i></a> </li>
+                      <li><a href="https://www.linkedin.com/"><i class="fa fa-2x fa-linkedin-square"></i></a> </li>
+                      <li><a href="https://twitter.com/"><i class="fa fa-2x fa-twitter-square"></i></a> </li>
+                    </ul> 
+                  </center>
+                </div>
+              </div>
+              </div>
             </div>
+            
             <div class="panel-body">
             </div>
             <?php require "social.php" ?>
