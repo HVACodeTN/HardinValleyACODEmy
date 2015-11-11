85c6f0bbe1894ff1dfec09fd5bb62beebcc17704
Sean Toll <smtdoo@comcast.net>
2015-11-11 09:23:22 -0500

    Update to about

--- apps/hvcodecademy/public/views/about.php
+++ apps/hvcodecademy/public/views/about.php
@@ -39,8 +39,8 @@
 	<br>
 	<h2>About the Project</h2>
 	<p>
-	To solve this year's problem, we created a School schedule and map to help schools
-    with simple tasks
+	To solve this year's problem, we created a school scheduling and map to help schools 
+  keep track of things around school, such as laptop carts and room assignments.
 	</p>
 	<br>
 	<h2>About the Team</h2>
@@ -58,18 +58,94 @@
 	      <h3> Tym Brandel
           <br>
 	        <small>Team Leader</small> </h3>
-	      <p>L</p>
-	      <ul class="list-inline">
+	      <p>Tym primarily spent his time working on HTML code when he was not guiding the team as a whole.</p>
+<!-- 	      <ul class="list-inline">
 	        <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a> </li>
 	        <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a> </li>
 	        <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a> </li>
-	        </ul>
+        </ul> -->
 	      </div>
 	    </div>
 	  <br>
 	  </p>
 	<br><br><br>
   </div>
+  
+  <div class="col-sm-2 text-left">
+  	  <div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+  	    <div class="caption">
+  	      <h3> Jackson Smith
+            <br>
+  	        <small>PHP Extraordinairre</small> </h3>
+  	      <p>Jackson was the master of the PHP arts, filling out the database with style.</p>
+<!--   	      <ul class="list-inline">
+  	        <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a> </li>
+  	        <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a> </li>
+  	        <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a> </li>
+          </ul> -->
+  	      </div>
+  	    </div>
+  	  <br>
+  	  </p>
+  	<br><br><br>
+    </div>
+    
+    <div class="col-sm-2 text-left">
+    	  <div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+    	    <div class="caption">
+    	      <h3> Sean Toll
+              <br>
+    	        <small>CSS Person</small> </h3>
+    	      <p>Sean made the websites presentable in a time when they looked bland.</p>
+<!--     	      <ul class="list-inline">
+    	        <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a> </li>
+    	        <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a> </li>
+    	        <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a> </li>
+            </ul> -->
+    	      </div>
+    	    </div>
+    	  <br>
+<!--     	  </p> -->
+    	<br><br><br>
+      </div>
+      
+      <div class="col-sm-2 text-left">
+      	  <div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+      	    <div class="caption">
+      	      <h3> Joey Townsend
+                <br>
+      	        <small>Database Person</small> </h3>
+      	      <p>Jpey assisted Jackson in his PHP objectives.</p>
+<!--       	      <ul class="list-inline">
+      	        <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a> </li>
+      	        <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a> </li>
+      	        <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a> </li>
+              </ul> -->
+      	      </div>
+      	    </div>
+      	  <br>
+<!--       	  </p> -->
+      	<br><br><br>
+        </div>
+        
+        <div class="col-sm-2 text-left">
+        	  <div class="thumbnail"> <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+        	    <div class="caption">
+        	      <h3> Jack Anderson
+                  <br>
+        	        <small>HTML Editor</small> </h3>
+        	      <p>Jack was there, too.</p>
+         	      <ul class="list-inline">
+        	        <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a> </li>
+        	        <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a> </li>
+        	        <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a> </li>
+                </ul> 
+        	      </div>
+        	    </div>
+        	  <br>
+        	  </p>
+        	<br><br><br>
+          </div>
   
   </center>
 
