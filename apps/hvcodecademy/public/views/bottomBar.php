8cbcd48bdff97fdf5d2de04b4ad62ffaeb1f7727
Tymethy <kootra6@gmail.com>
2015-11-11 15:10:18 -0500

    Navbar and Footer fixed.
    Navbar no longer disappears if window is to small and footer text is no
    longer causing the footer size to enlarge.

--- apps/hvcodecademy/public/views/bottomBar.php
+++ apps/hvcodecademy/public/views/bottomBar.php
@@ -1,19 +1,23 @@
-<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+ <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
     <div class="container-fluid" style="background-color:#00246B">
-        <li>
-            <font color="white">
-                <p>HardinValleyACODEmy@gmail.com</p>
-                <p>2015 | By: Hardin Valley ACODEmy</p>
+      
+      <font color="white">
+             <center>   <p style="position:relative; height: 10px;" >HardinValleyACODEmy@gmail.com 2015 
+                <br />
+                 By: Hardin Valley ACODEmy</p>
+               </center>
             </font>
-        </li>
+     
         <ul class="nav navbar-nav navbar-right">
+
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+             
                 <ul class="nav navbar-nav">
                     <li><a href="index.php" style="background-color:#00246B">Home</a></li>
                     <li><a href="about.php"><font color="#999999">About The Team</font></a></li>
                 </ul>
             </div><!-- /.navbar-collapse -->
         </ul>
-    </div><!-- /.container-fluid -->
+    </div>
 </nav>
--- apps/hvcodecademy/public/views/navHeader.php
+++ apps/hvcodecademy/public/views/navHeader.php
@@ -6,14 +6,20 @@ if ($_SESSION['user']['AccountType'] == 'Administrator'){
 <img src="\views\assets\img\Header.jpg" alt="LOGO" style="width:100%;height:10%"/>
 
 <!-- LOGO HEADER END-->
-    <section class="menu-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="navbar-collapse collapse ">
-                        <ul id="menu-top" class="nav navbar-nav navbar-left ">
-                           
-                            <li><a href="index.php">Home</a></li>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="#">Hardin Valley ACODEmy</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        
+ 			    			<li><a href="index.php">Home</a></li>
                             <li><a href="about.php">About Us</a></li>
                             <li><a href="Map_Schedule.php">Schedule</a></li>
                             <li><a href="ItemSignUp.php">Cart SignUp</a></li>
@@ -23,12 +29,9 @@ if ($_SESSION['user']['AccountType'] == 'Administrator'){
                             <?php endif; ?>                    
                             <li><a href="edit_account.php">Edit Account</a></li>
                             <li><a href="Logout.php">Logout</a></li>
+                            
+      </ul>
+    </div>
+  </div>
+</nav>
 
-                        </ul>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section>
-<!-- MENU SECTION END-->
