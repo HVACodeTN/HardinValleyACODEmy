6a3c703dd8853b54b6770c18db458352c769427d
rjsmith1999 <rjsmith1999@gmail.com>
2015-11-11 16:40:22 -0500

    Format improvements to bottom bar

--- apps/hvcodecademy/public/views/bottomBar.php
+++ apps/hvcodecademy/public/views/bottomBar.php
@@ -1,18 +1,21 @@
  <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
     <div class="container-fluid" style="background-color:#00246B">
-      
+
       <font color="white">
-             <center>   <p style="position:relative; height: 10px;" >HardinValleyACODEmy@gmail.com 2015 
-                <br />
-                 By: Hardin Valley ACODEmy</p>
-               </center>
-            </font>
-     
+            <center>
+                <p style="position:relative; height: 10px;" >
+                    HardinValleyACODEmy@gmail.com 2015
+                    <br />
+                    By: Hardin Valley ACODEmy
+                </p>
+            </center>
+        </font>
+
         <ul class="nav navbar-nav navbar-right">
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-             
+
                 <ul class="nav navbar-nav">
                     <li><a href="index.php" style="background-color:#00246B">Home</a></li>
                     <li><a href="about.php"><font color="#999999">About The Team</font></a></li>
