3b50e18b290b3b1971568c26272d35e8c5054135
rjsmith1999 <rjsmith1999@gmail.com>
2015-11-12 17:08:29 -0500

    Format Fixes

--- apps/hvcodecademy/public/views/Map_Schedule.php
+++ apps/hvcodecademy/public/views/Map_Schedule.php
@@ -1,76 +1,78 @@
 <?php
 
-    require("private.php");
+require("private.php");
 
-    require("roomProcessor.php");
+require("roomProcessor.php");
 
-    //TODO: Jackson create variable for each room with "TeacherName" + "Room name" from database based on current time.
-    // Rooms include All Classrooms, Labs, Workrooms, Multipurpose room, Library, bus duty (It's a place on the map)
-    //$D101="TeacherName" + " D101";
+//TODO: Jackson create variable for each room with "TeacherName" + "Room name" from database based on current time.
+// Rooms include All Classrooms, Labs, Workrooms, Multipurpose room, Library, bus duty (It's a place on the map)
+//$D101="TeacherName" + " D101";
 
-    //TODO: Logic to chose Period
-    $period = 2;
+//TODO: Logic to chose Period
+$period = 2;
 
-    //Create Query
-    $query = "SELECT
-                Schedule.Room,
-                Users.UserName,
-                Rooms.RoomName
-            FROM Schedule,Users,Rooms
-            WHERE
-                Schedule.Period = :Period AND
-                Users.UserID = Schedule.UserID AND
-                Rooms.RoomNumber = Schedule.Room";
-    $query_params = array(
-        ':Period' => $period
-    );
+//Create Query
+$query = "SELECT
+    Schedule.Room,
+    Users.UserName,
+    Rooms.RoomName,
+    CartCheckout.
+    FROM Schedule,Users,Rooms,CartCheckout
+    WHERE
+    Schedule.Period = :Period AND
+    Users.UserID = Schedule.UserID AND
+    Rooms.RoomNumber = Schedule.Room
+";
+$query_params = array(
+    ':Period' => $period
+);
 
 
-    $num_results = 0;
-    try
-    {
-        // Execute the query against the database
-        $stmt = $db->prepare($query);
-        $result = $stmt->execute($query_params);
-        $num_results = $stmt->rowCount();
-    }
-    catch(PDOException $ex)
-    {
-        //display if failed to run
-        die("Failed to run query: " . $ex->getMessage());
-    }
+$num_results = 0;
+try
+{
+    // Execute the query against the database
+    $stmt = $db->prepare($query);
+    $result = $stmt->execute($query_params);
+    $num_results = $stmt->rowCount();
+}
+catch(PDOException $ex)
+{
+    //display if failed to run
+    die("Failed to run query: " . $ex->getMessage());
+}
 
-    //Radio Buttons:
-    // Down:B/C/D/E/F
-    // UP F/E/D
-    // Bus: A/Bus
-    switch ($Location) {
-        case 'Down':
-            # code...
-            break;
-        case 'UP':
-            # code...
-            break;
-        case 'Bus':
-            # code...
-            break;
-        default:
-            # code...
-            break;
-    }
-    // Iterate Results
-    // for ($i=0; $i < $num_results; $i++) {
-    //     //Process Results
-    //     $row = $stmt->fetch();
-    //
-    //     // Use data to create variable for schedule
-    //     $varName = roomString($row['Room']);
-    //     global $$varName,$varName;
-    //     $$varName = $row['Name']; // Takes row and makes it a variable with teacher name in it.
-    //     echo $varName.": ".$$varName."\n"; //FIXME: Temp debug echo
-    // }
+//Radio Buttons:
+// Down:B/C/D/E/F
+// UP F/E/D
+// Bus: A/Bus
+switch ($Location) {
+    case 'Down':
+    # code...
+    break;
+    case 'UP':
+    # code...
+    break;
+    case 'Bus':
+    # code...
+    break;
+    default:
+    # code...
+    break;
+}
+// Iterate Results
+// for ($i=0; $i < $num_results; $i++) {
+//     //Process Results
+//     $row = $stmt->fetch();
+//
+//     // Use data to create variable for schedule
+//     $varName = roomString($row['Room']);
+//     global $$varName,$varName;
+//     $$varName = $row['Name']; // Takes row and makes it a variable with teacher name in it.
+//     echo $varName.": ".$$varName."\n"; //FIXME: Temp debug echo
+// }
 
-    //Put text on map
+//Put text on map
 
 
 ?>
@@ -96,80 +98,74 @@
                 <br />
                 <br />
                 <script type="text/javascript">
-		  function ChangeBackground(bg){
-		  	var url;
-			var Asection;
-			if(bg=="1stfloor")
-				{
-					url="assets/img/1stfloor.jpg";
-					document.getElementById("map").style.backgroundImage = "url(" + url + ")";
-				}
-					else if(bg=="2ndfloor")
-						{
-							url="assets/img/2ndfloor.jpg";
-							document.getElementById("map").style.backgroundImage = "url(" + url + ")";
-						}
-							else if(bg=="buspickup")
-								{
-									url="assets/img/buspickup.jpg";
-									document.getElementById("map").style.backgroundImage = "url(" + url + ")";
-									/*$("input[type='radio']").change(function(){
-										if($(this).val()=="buspickup")
-										{
-											$("buspickup").show("buspickup");
-										}
-										else
-										{
-											$("1stfloor|| 2ndfloor").hide("buspickup");
-										}
-									});*/
-
-								}
-		  }
-								</script>
+                function ChangeBackground(bg){
+                    var url;
+                    var Asection;
+                    if(bg=="1stfloor")
+                    {
+                        url="assets/img/1stfloor.jpg";
+                        document.getElementById("map").style.backgroundImage = "url(" + url + ")";
+                    }
+                    else if(bg=="2ndfloor")
+                    {
+                        url="assets/img/2ndfloor.jpg";
+                        document.getElementById("map").style.backgroundImage = "url(" + url + ")";
+                    }
+                    else if(bg=="buspickup")
+                    {
+                        url="assets/img/buspickup.jpg";
+                        document.getElementById("map").style.backgroundImage = "url(" + url + ")";
+                        /*$("input[type='radio']").change(function(){
+                            if($(this).val()=="buspickup") {
+                                $("buspickup").show("buspickup");
+                            } else {
+                                $("1stfloor|| 2ndfloor").hide("buspickup");
+                            }
+                        });*/
+                    }
+                }
+                </script>
             </div>
             <div class="panel-body">
-
                 <!-- Add PHP Table -->
                 <h1>Schedule</h1>
                 <table class="table table-border">
-            <thead>
-                <tr>
-                    <th>Room</th>
-                    <th>Username</th>
-                    <th>Item</th>
-                    <th>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php //Iterate Results
-                    for ($i=0; $i < $num_results; $i++):
-                        //Process Results
-                        $row = $stmt->fetch();?>
+                    <thead>
                         <tr>
-                            <td><?php
-                            if ($row['RoomName']) {
-                                echo htmlentities($row['RoomName'], ENT_QUOTES, 'UTF-8');
-                            } else {
-                                echo roomString($row['Room']);
-                            } ?></td>
-                            <td><?php echo htmlentities($row['UserName'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <th>Room</th>
+                            <th>Username</th>
+                            <th>Item</th>
+                            <th>Email</th>
                         </tr>
-                    <?php endfor; ?>
-		</tbody>
+                    </thead>
+                    <tbody>
+                        <?php //Iterate Results
+                        for ($i=0; $i < $num_results; $i++):
+                            //Process Results
+                            $row = $stmt->fetch();?>
+                            <tr>
+                                <td><?php
+                                if ($row['RoomName']) {
+                                    echo htmlentities($row['RoomName'], ENT_QUOTES, 'UTF-8');
+                                } else {
+                                    echo roomString($row['Room']);
+                                } ?></td>
+                                <td><?php echo htmlentities($row['UserName'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            </tr>
+                        <?php endfor; ?>
+                    </tbody>
                 </table>
                 <!-- End PHP Table -->
             </div>
         </div>
-     </div>
-            <?php require "social.php" ?>
+    </div>
+    <?php require "social.php" ?>
 
-            <!-- CONTENT-WRAPPER SECTION END-->
-       </div>
-    	<?php require "bottomBar.php" ?>
+    <!-- CONTENT-WRAPPER SECTION END-->
+    <?php require "bottomBar.php" ?>
 
 
-		<?php require "LinkScript.php" ?>
+    <?php require "LinkScript.php" ?>
 
 </body>
 </html>
