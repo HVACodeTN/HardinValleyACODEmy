c8f1e323503b5e66e6e3c61bb2e88184c0a0e632
Tymethy <kootra6@gmail.com>
2015-11-12 14:45:09 -0500

    Map page table and cart values
    table started for the map page and Cart and other values added to the
    cart signup page.

--- apps/hvcodecademy/public/views/ItemSignUp.php
+++ apps/hvcodecademy/public/views/ItemSignUp.php
@@ -57,33 +57,7 @@ if ($_SESSION['user']['AccountType']=='Teacher') {
 <?php require 'Link.php'; ?>
 
 =======
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <!--[if IE]>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <![endif]-->
-    <title>Access Item Signup</title>
-    <!-- BOOTSTRAP CORE STYLE  -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <!-- FONT AWESOME ICONS  -->
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <!-- CUSTOM STYLE  -->
-    <link href="assets/css/style.css" rel="stylesheet" />
-    <!-- DATA SELECTOR BOOTSTRAPS -->
-    <!-- <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"> -->
-    <link rel="stylesheet" type="text/css" media="screen" href="http://tarruda.github.com/bootstrap-datetimepicker/assets/css/bootstrap-datetimepicker.min.css">
-     <!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
+
 >>>>>>> origin/master
 <body>
 
@@ -196,21 +170,21 @@ if ($_SESSION['user']['AccountType']=='Teacher') {
                 	       <label for="">Item:</label>
                            <input id="" name="select item" type="text" list="SignOutItems" />
                                 <datalist id="SignOutItems" placeholder="select">
-                                    <option value="Cart" label="15 Labtops">
-                                    <option value="">
-                                    <option value="">
-                                    <option value="">
-                                    <option value="">
-                                    <option value="">
-                                    <option value="">
-                                    <option value="">
-                                    <option value="">
-                                    <option value="">
-                                    <option value="">
-                                    <option value="">
-                                    <option value="">
-                                    <option value="">
-                                    <option value="">
+                                    <option value="A Cart" label="15 Labtops">
+                                    <option value="B Cart" label="15 Labtops">
+                                    <option value="C Cart" label="15 Labtops">
+                                    <option value="D Cart" label="15 Labtops">
+                                    <option value="E Cart" label="15 Labtops">
+                                    <option value="F Cart" label="15 Labtops">
+                                    <option value="G Cart" label="30 Labtops">
+                                    <option value="H Cart" label="30 Labtops">
+                                    <option value="I Cart" label="30 Labtops">
+                                    <option value="Special Ed 1" label="15 Labtop">
+                                    <option value="Special Ed 2" label="15 Labtop">
+                                    <option value="Ipad Cart" Label="10 Ipads">
+                                    <option value="Tv" label="example">
+                                    <option value="Projector" label="example">
+                                    <option value="Lab Equipment" label="example">
                                     <option value="">
                                     <option value="">
                                     <option value="">
